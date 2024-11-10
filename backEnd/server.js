58d3d8caa55b115d1c86a0f95e4260707fba18b0
Barani1912 <baranivignesh0024@gmail.com>
2024-11-10 17:51:05 +0530

    made changes for deployment

--- backEnd/server.js
+++ backEnd/server.js
@@ -29,10 +29,10 @@ app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 app.use("/api/users",userRoutes)
 
-app.use(express.static(path.join(__dirname, "../frontEnd/dist")));
+app.use(express.static(path.join(__dirname, "frontEnd/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../frontEnd","dist","index.html"));
+	res.sendFile(path.join(__dirname, "frontEnd","dist","index.html"));
 });
 
 server.listen(PORT,()=>{
