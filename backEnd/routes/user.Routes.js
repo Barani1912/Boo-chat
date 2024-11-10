f882c22e2bed07b47bdc246660909bc4fbea73d9
Barani1912 <baranivignesh0024@gmail.com>
2024-11-10 19:16:02 +0530

    Common js into Es module

--- backEnd/routes/user.Routes.js
+++ backEnd/routes/user.Routes.js
@@ -2,11 +2,11 @@
 
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar } from "../controllers/user.Controller.js";
+import { getUsersForSideBar } from "../controllers/user.Controller.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getUsersForSidebar);
+router.get("/", protectRoute, getUsersForSideBar);
 
 export default router;
 
