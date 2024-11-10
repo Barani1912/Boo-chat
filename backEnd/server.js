a87515d76cde00917998d491b6a7fe6380b17bdc
Barani1912 <baranivignesh0024@gmail.com>
2024-11-10 18:40:12 +0530

    Common js into Es module

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,17 +1,16 @@
 //1:38:00 -> backend part1 ends here...
- 
-const express = require("express");
-const path = require("path") ;
-const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
 
-const authRoutes = require("./routes/auth.Routes.js");
-const messageRoutes = require("./routes/message.Routes.js");
-const userRoutes = require("./routes/user.Routes.js");
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-const connectToMongoDb = require("./db/mongodbconnect.js");
-const {app,server} = require("./socket/socket.js")
+import authRoutes from "./routes/auth.Routes.js";
+import messageRoutes from "./routes/message.Routes.js";
+import userRoutes from "./routes/user.Routes.js";
 
+import connectToMongoDb from "./db/mongodbconnect.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
@@ -25,17 +24,63 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
-app.use("/api/message",messageRoutes)
-app.use("/api/users",userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname,"/frontEnd/dist")));
+app.use(express.static(path.join(__dirname, "/frontEnd/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname,"frontEnd","dist","index.html"));
+  res.sendFile(path.join(__dirname, "frontEnd", "dist", "index.html"));
 });
 
-server.listen(PORT,()=>{
-    connectToMongoDb();
-    console.log(`The Server Is Running On Port ${PORT}`)
+server.listen(PORT, () => {
+  connectToMongoDb();
+  console.log(`The Server Is Running On Port ${PORT}`);
 });
+
+
+
+///////////
+
+// //1:38:00 -> backend part1 ends here...
+ 
+// const express = require("express");
+// const path = require("path") ;
+// const dotenv = require("dotenv");
+// const cookieParser = require("cookie-parser");
+
+// const authRoutes = require("./routes/auth.Routes.js");
+// const messageRoutes = require("./routes/message.Routes.js");
+// const userRoutes = require("./routes/user.Routes.js");
+
+// const connectToMongoDb = require("./db/mongodbconnect.js");
+// const {app,server} = require("./socket/socket.js")
+
+
+// dotenv.config();
+
+// const __dirname = path.resolve();
+// //In CommonJS, __dirname is automatically available, so there's no need to redefine it
+
+// // const app = express();
+// const PORT = process.env.PORT || 5000;
+// // process.env.PORT ...it wont work without importing and cofiguring...
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use("/api/auth",authRoutes)
+// app.use("/api/message",messageRoutes)
+// app.use("/api/users",userRoutes)
+
+// app.use(express.static(path.join(__dirname,"/frontEnd/dist")));
+
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname,"frontEnd","dist","index.html"));
+// });
+
+// server.listen(PORT,()=>{
+//     connectToMongoDb();
+//     console.log(`The Server Is Running On Port ${PORT}`)
+// });
